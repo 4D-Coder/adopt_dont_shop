171e174cf8c1fa6d2251e3459cce92005d5667fa
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-02-11 12:02:41 -0700

    Fix/Add: Started user story 5 and removed lingering prys in applications show page

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -9,20 +9,16 @@ class ApplicationsController < ApplicationController
 
   def create 
     application = Application.new(application_params)
-  require 'pry'; binding.pry
 
     if application.save
       redirect_to "/applications/#{application.id}" 
-      require 'pry'; binding.pry
     else
-      require 'pry'; binding.pry
       redirect_to "/applications/new"
       @errors = application.errors.messages
 
       flash[:alert] = @errors.map do |error|
         "#{error.first.capitalize} #{error.last}.".gsub(/[\["\]]/, "")
       end.join.gsub(/\./, ". ")
-      require 'pry'; binding.pry
     end 
   end
 
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -21,35 +21,44 @@ RSpec.describe 'applications show page features' do
         expect(page).to have_content("Status: In_Progress")
         expect(page).to have_content("Description: #{@applicant_1.description}")
       end
+      
+      
+    end
 
-      context "the application has not yet been submitted" do 
-        it "has a section on the page to 'Add a Pet to this Application'" do 
-          visit "/applications/#{@applicant_1.id}"
+    context "the application has not yet been submitted" do 
+      it "has a section on the page to 'Add a Pet to this Application'" do 
+        visit "/applications/#{@applicant_1.id}"
 
-          expect(page).to have_content("Add a Pet to this Application")
+        expect(page).to have_content("Add a Pet to this Application")
 
-        end
-
-        it "that section has an input where I can search for Pets by name" do 
-
-          visit "/applications/#{@applicant_1.id}"
-          expect(page).to have_selector("form")
-
-        end
-
-        it "when I fill in this field with a Pet's name and click submit, I am taken back to the application show page and under the search bar I see any pet whose name matches my search" do 
-          visit "/applications/#{@applicant_1.id}"
-          fill_in "search", with: "Norma"
-          click_button "Search"
-          save_and_open_page
-          expect(current_path).to eq("/applications/#{@applicant_1.id}")
-
-          expect(page).to have_content("Norma")
-          expect("Pets Returned by Search").to appear_before("Norma")
-
-        end
       end
 
+      it "that section has an input where I can search for Pets by name" do 
+
+        visit "/applications/#{@applicant_1.id}"
+        expect(page).to have_selector("form")
+
+      end
+
+      it "when I fill in this field with a Pet's name and click submit, I am taken back to the application show page and under the search bar I see any pet whose name matches my search" do 
+        visit "/applications/#{@applicant_1.id}"
+        fill_in "search", with: "Norma"
+        click_button "Search"
+        save_and_open_page
+        expect(current_path).to eq("/applications/#{@applicant_1.id}")
+
+        expect(page).to have_content("Norma")
+        expect("Pets Returned by Search").to appear_before("Norma")
+      end
+
+      it 'displays a button to adopt a pet' do
+        visit "/applications/#{@applicant_1.id}"
+        fill_in "search", with: "Norma"
+        click_button "Search"
+
+        expect(page).to have_button("Adopt this Pet")
+      end
     end
+
   end
 end
