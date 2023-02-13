d08484f3361872966cce172bdcc4dc28ee7a96f5
Hady Matar <hadyematar@gmail.com>
2023-02-13 15:57:55 -0700

    Finished troubleshooting 13

--- app/controllers/admin/application_pets_controller.rb
+++ app/controllers/admin/application_pets_controller.rb
@@ -2,11 +2,13 @@ module Admin
   class ApplicationPetsController < ApplicationController
     def update
       application_pet = ApplicationPet.where(application_id: params[:id]).where(pet_id: params[:pet_id])
-
+      require 'pry'; binding.pry
       if params[:approval] == "approved"
         application_pet.update approval: 1
+        require 'pry'; binding.pry
       elsif params[:approval] == "denied"
         application_pet.update approval: 2
+        require 'pry'; binding.pry
       end 
 
       redirect_to "/admin/applications/#{params[:id]}?"
